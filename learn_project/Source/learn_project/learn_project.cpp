3371f3943ad264e92fc8d4fda69e464abf20bc2d
Maks_Nik_CPP7 <odionyx@yandex.ru>
2024-02-14 21:26:15 +0400

    Change learn_project/

---
+++ learn_project/Source/learn_project/learn_project.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "learn_project.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, learn_project, "learn_project" );
