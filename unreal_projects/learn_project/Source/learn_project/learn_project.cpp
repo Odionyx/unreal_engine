0a651c3efc98d335f4be2807420ca86a42bb4221
Odionyx <104016005+Odionyx@users.noreply.github.com>
2024-02-14 21:30:43 +0400

    Delete unreal_projects directory

--- unreal_projects/learn_project/Source/learn_project/learn_project.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "learn_project.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, learn_project, "learn_project" );
